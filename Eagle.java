579bdd43db00ae1360d5561617cff0a952a2cdb3
swann <chevreauswann@outlook.fr>
2020-03-27 11:10:48 +0100

    quête 8

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -8,6 +8,8 @@ public class Eagle extends Bird {
         this.flying = false;
         this.altitude = 0;
     }
+
+    
 
     public int getAltitude() {
         return altitude;
@@ -20,5 +22,49 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+
+    @Override 
+    public void takeOff() {
+        if (!this.flying && this.altitude == 0) {
+            this.flying = true;
+            System.out.println(this.getName() + " takes off in the sky");
+        }
+    }
+
+    @Override
+    public int ascend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.min(this.altitude + meters, 325);
+            System.out.println(this.getName() + " flies upward, altitude : " + this.altitude);
+        }
+        return this.altitude;
+    } 
+
+    
+    
+    @Override
+    public void glide() {
+        System.out.println("It glides into the air.");
+    }
+
+    @Override
+    public int descend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.max(this.altitude - meters, 0);
+            System.out.println(this.getName() + "flies downward, altitude : " + this.altitude);
+        }
+        return this.altitude;
+    }
+
+    @Override
+    public void land() {
+        if (this.altitude > 9) {
+            System.out.println(this.getName() + "is too hight, it can't lands");
+        }
+        else{
+            System.out.println(this.getName() + " lands on the ground");
+        }
     }
 }
---
+++ Fly.java
@@ -0,0 +1,12 @@
+public interface Fly {
+
+    void takeOff();
+
+    int ascend(int meters);
+
+    void glide();
+
+    int descend( int meters);
+
+    void land();
+}
